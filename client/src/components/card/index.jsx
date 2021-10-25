a7f4d01c3d2c69b02c7db5d4eab75cdf54b4cd3b
Daniel <danpark105@gmail.com>
2021-10-25 02:45:26 -0400

    adding server

---
+++ client/src/components/card/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./card.css";
+import Heart from "../../img/heart.svg";
+import HeartFilled from "../../img/heartFilled.svg";
+import Comment from "../../img/comment.svg";
+import Share from "../../img/share.svg";
+import Info from "../../img/info.svg";
+
+const Card = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(true);
+  };
+
+  const handleUnlike = () => {
+    setLiked(false);
+  };
+  return (
+    <div className="card">
+      <div className="info">
+        <img src={post.userImg} alt="" className="userImg" />
+        <span>{post.fullname}</span>
+      </div>
+      <img src={post.postImg} alt="" className="postImg" />
+      <div className="interaction">
+        {liked ? (
+          <img
+            src={HeartFilled}
+            alt=""
+            className="cardIcon"
+            onClick={handleUnlike}
+          />
+        ) : (
+          <img src={Heart} alt="" className="cardIcon" onClick={handleLike} />
+        )}
+        <img src={Comment} alt="" className="cardIcon" />
+        <img src={Share} alt="" className="cardIcon" />
+        <img src={Info} alt="" className="cardIcon infoIcon" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
