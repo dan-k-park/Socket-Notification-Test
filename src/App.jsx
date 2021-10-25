8bf50cb8f34f47b3207aae20ec15c9ee3fbccc5d
Daniel <danpark105@gmail.com>
2021-10-24 21:57:37 -0400

    creating base app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import Card from "./components/card";
+import Navbar from "./components/navbar";
+import "./App.css";
+import { posts } from "./data";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
   return (
     <div className="container">
-      
+      {user ? (
+        <>
+          <Navbar />
+          {posts.map((post) => (
+            <Card key={post.id} post={post} />
+          ))}
+          <span className="username">{user}</span>
+        </>
+      ) : (
+        <div className="login">
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={() => setUser(username)}>Login</button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
